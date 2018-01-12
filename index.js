42a5ec69cce8ab58799a6e9e61661b4aca160944
Eric Mendes Dantas <ericdantas0@gmail.com>
2018-01-12 09:29:47 -0200

    .

--- index.js
+++ index.js
@@ -6,29 +6,14 @@
 			function() {
 				let itemsCache = [];
 				this.items = [];
-				this.qtd = 100000; // 100k
+				this.qtd = 25000; // 25k
 
-				this.push = function(qtd) {
+				this.clear = function() {
 					this.items.length = 0;
 					itemsCache.length = 0;
-
-					console.time('push')
-					for (let i = 0; i < qtd; i++) {
-						this.items.push({
-							_id: i,
-							info1: 'a' + i,
-							info2: 'b' + i,
-							info3: 'c' + i,
-						});
-					}
-					console.timeEnd('push')
 				}
 
-				this.ref = function(qtd) {
-					this.items.length = 0;
-					itemsCache.length = 0;
-
-					console.time('ref')
+				this.add = function(qtd) {
 					for (let i = 0; i < qtd; i++) {
 						itemsCache.push({
 							_id: i,
@@ -37,7 +22,6 @@
 							info3: 'c' + i,
 						});
 					}
-					console.timeEnd('ref')
 
 					this.items = itemsCache;
 				}
