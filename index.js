561afc57acb5d020e66badb44013fc24fae94f3e
Eric Mendes Dantas <ericdantas0@gmail.com>
2018-01-12 11:26:50 -0200

    .

--- index.js
+++ index.js
@@ -3,17 +3,45 @@
 
 	ng.module('app', [])
 	  .controller('Ctrl', [
-			function() {
+			'$interval',
+			function($interval) {
 				let itemsCache = [];
+				let intervalP = null;
 				this.items = [];
-				this.qtd = 25000; // 25k
+				this.qtd = 250000; // 250k
 
 				this.clear = function() {
 					this.items.length = 0;
 					itemsCache.length = 0;
+
+					$interval.cancel(intervalP)
 				}
 
-				this.add = function(qtd) {
+				function debounce(itemsCache, time, cb) {
+					let initPos = 0
+					let lastPos = 0
+					let walk = 25000
+					let itemsCacheBroken = []
+
+					intervalP = $interval(() => {
+						initPos = lastPos
+						lastPos += walk
+
+						if (!itemsCache[lastPos]) {
+							$interval.cancel(intervalP)
+						}
+
+						for (let i = initPos; i < lastPos; i++) {
+							itemsCacheBroken.push(itemsCache[i])
+						}
+
+						cb(itemsCacheBroken)
+
+						itemsCacheBroken.length = 0
+					}, time)
+				}
+
+				this.addBatch = function(qtd) {
 					for (let i = 0; i < qtd; i++) {
 						itemsCache.push({
 							_id: i,
@@ -23,7 +51,22 @@
 						});
 					}
 
-					this.items = itemsCache;
+					debounce(itemsCache, 33, (itemsCacheBroken) => {
+						this.items = this.items.concat(itemsCacheBroken)
+					})
+				}
+
+				this.addAll = function(qtd) {
+					for (let i = 0; i < qtd; i++) {
+						itemsCache.push({
+							_id: i,
+							info1: 'a' + i,
+							info2: 'b' + i,
+							info3: 'c' + i,
+						});
+					}
+
+					this.items = itemsCache
 				}
 			}
 	  ]);
